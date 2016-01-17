92fd443290c9902d8ce9b9b9d7b330c555ff730a
nimaghorbani <nimaghorbani@users.noreply.github.com>
2016-01-17 05:54:58 -0800

    Update suport.lua

--- plugins/suport.lua
+++ plugins/suport.lua
@@ -1,6 +1,6 @@
 do
     function run(msg, matches)
-	return 'For Join To Suport Gp Thor-Bot \n Click on Under Link :  Creator: @Hoo3instar'
+	return 'For Join To Suport Gp Thor-Bot \n Click on Under Link :   \n Creator: @Hoo3instar'
 end
 return {
   patterns = {
