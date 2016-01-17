5beff2dd477817a288aed39f6352053c4e856223
nimaghorbani <nimaghorbani@users.noreply.github.com>
2016-01-17 05:54:37 -0800

    Create suport.lua

---
+++ plugins/suport.lua
@@ -0,0 +1,12 @@
+do
+    function run(msg, matches)
+	return 'For Join To Suport Gp Thor-Bot \n Click on Under Link :  Creator: @Hoo3instar'
+end
+return {
+  patterns = {
+      "^[/!%&$]([S|s]uport)$",
+      "^([S|s]uport)$"
+      }, 
+  run = run 
+}
+end
