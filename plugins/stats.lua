7f379e9ddb8aeb452a7947bf4aba4022a1bd6818
nimaghorbani <nimaghorbani@users.noreply.github.com>
2016-01-18 07:07:11 -0800

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -91,10 +91,10 @@ local function bot_stats()
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'satan' then -- Put everything you like :)
+  if matches[1]:lower() == 'silver' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
-    savelog(msg.to.id, name.." ["..msg.from.id.."] used /satan ")
+    savelog(msg.to.id, name.." ["..msg.from.id.."] used /silver ")
     return about
   end 
   if matches[1]:lower() == "statslist" then
@@ -120,7 +120,7 @@ local function run(msg, matches)
         return
       end
     end
-    if matches[2] == "satan" then -- Put everything you like :)
+    if matches[2] == "silver" then -- Put everything you like :)
       if not is_admin(msg) then
         return "For admins only !"
       else
@@ -141,13 +141,13 @@ return {
     "^[!/$&]([Ss]tats)$",
     "^[!/$&]([Ss]tatslist)$",
     "^[!/$&]([Ss]tats) (group) (%d+)",
-    "^[!/$&]([Ss]tats) (satan)",
-    "^([Ss]atan)",
+    "^[!/$&]([Ss]tats) (silver)",
+    "^([Ss]ilver)",
     "^([Ss]tats)$",
     "^([Ss]tatslist)$",
     "^([Ss]tats) (group) (%d+)",
-    "^([Ss]tats) (satan)",
-    "^([Ss]atan)"
+    "^([Ss]tats) (silver)",
+    "^([Ss]ilver)"
     }, 
   run = run
 }
