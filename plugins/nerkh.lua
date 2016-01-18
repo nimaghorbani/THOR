74a8ef4ddbbe970bb70421a3be46826e13945d90
nimaghorbani <nimaghorbani@users.noreply.github.com>
2016-01-18 07:10:50 -0800

    Update nerkh.lua

--- plugins/nerkh.lua
+++ plugins/nerkh.lua
@@ -1,11 +1,11 @@
 do
     function run(msg, matches)
-	return 'Nerkh Sakht GP 1 Mahe \n 2000 Toman Kart Sharj \n Nerkh Sakht GP Daemi \n 5000 Toman Kart Sharj \n Creator: @Hoo3instar'
+	return 'Nerkh Sakht GP 1 Mahe \n 2000 Toman Kart Sharj \n Nerkh Sakht GP Daemi \n 5000 Toman Kart Sharj \n Creator: @Mr_SilVer1 & @MrGhor'
 end
 return {
   patterns = {
-      "^[/!%&$]([N|n]erkh)$",
-      "^([N|n]erkh)$"
+      "^[/!%&$]([Nn]erkh)$",
+      "^([Nn]erkh)$"
       }, 
   run = run 
 }
