db5b06fedd43d155444793961a7beaec1fb19fe2
ghKinG <nimaghorbani@users.noreply.github.com>
2016-01-18 09:20:09 -0800

    Update calc.lua

--- plugins/calc.lua
+++ plugins/calc.lua
@@ -23,8 +23,7 @@ return {
   description = "Calculate math expressions with mathjs API",
   usage = "!calc [expression]: evaluates the expression and sends the result.",
   patterns = {
-    "^[!/]([Cc]alc) (.*)$",
-    "^([Cc]alc) (.*)$"
+    "^[!/]([Cc]alc) (.*)$"
   },
   run = run
 }
